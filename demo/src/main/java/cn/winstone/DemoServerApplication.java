474f9e030ea1ff36de5764c2cb34cf3c4b9ab467
gaolei.wei 魏高磊 <gaolei.wei@qiyuesuo.com>
2021-03-04 15:41:20 +0800

    gateway,eureka,demo-server提交

--- demo/src/main/java/cn/winstone/DemoServerApplication.java
+++ demo/src/main/java/cn/winstone/DemoServerApplication.java
@@ -2,17 +2,16 @@ package cn.winstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Winstone
- * @date 2021/3/4 - 1:57 下午
+ * @date 2021/3/4 - 3:31 下午
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+@EnableEurekaClient
+public class DemoServerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args).start();
+		SpringApplication.run(DemoServerApplication.class,args);
 	}
 }
--- gateway/src/main/java/cn/winstone/GatewayApplication.java
+++ gateway/src/main/java/cn/winstone/GatewayApplication.java
@@ -2,17 +2,17 @@ package cn.winstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Winstone
- * @date 2021/3/4 - 1:57 下午
+ * @date 2021/3/4 - 3:15 下午
  */
+
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+@EnableEurekaClient
+public class GatewayApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args).start();
+		SpringApplication.run(GatewayApplication.class,args);
 	}
 }
